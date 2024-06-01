02c9eb0ae886a6bb4345fe80d71bb3b87aa42d21
Enes Diler <dilerenes@gmail.com>
2024-06-02 00:11:20 +0300

    helper updated

--- deployer/helper.go
+++ deployer/helper.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
@@ -64,7 +65,8 @@ func exportSSHKeyToHost(keyName, identifierType, identifier string) error {
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		body, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("unexpected status code: %d, response: %s", resp.StatusCode, string(body))
 	}
 
 	return nil
@@ -76,6 +78,7 @@ func setupSSHKeysForHosts(keyName string, hosts map[string]string) error {
 	}
 
 	for name, ip := range hosts {
+		common.Info("Exporting SSH key to host %s\n", name)
 		err := exportSSHKeyToHost(keyName, "ip", ip)
 		if err != nil {
 			common.Warn("Failed to export SSH key using IP for host %s: %v, trying hostname\n", name, err)
@@ -83,6 +86,8 @@ func setupSSHKeysForHosts(keyName string, hosts map[string]string) error {
 			if err != nil {
 				return common.Err("failed to export SSH key to host %s using both IP and hostname: %v", name, err)
 			}
+		} else {
+			common.Ok("Successfully exported SSH key to host %s using IP: %s\n", name, ip)
 		}
 	}
 
